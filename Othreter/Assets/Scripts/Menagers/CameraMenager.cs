09e4b4c4b7ad37fd5f6434c00bf380780922bee2
MItCHeLPL <45109417+MItCHeLPL@users.noreply.github.com>
2019-04-09 23:00:54 +0200

    Volumetryczne oświetlenie i mgła

---
+++ Othreter/Assets/Scripts/Menagers/CameraMenager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/* Keeps track of the player */
+
+public class CameraMenager : MonoBehaviour
+{
+    #region Singleton
+
+    public static CameraMenager instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    #endregion
+
+    public Camera cam;
+}
---
+++ Othreter/Assets/Scripts/UI/OverHeadUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class OverHeadUI : MonoBehaviour
+{
+	public TextMeshProUGUI healthUI;
+	private Camera cam;
+	private Vector3 pos;
+
+	void Start()
+    {
+		cam = ObjectsMenager.instance.cam;
+    }
+
+    void Update()
+    {
+		pos = cam.WorldToScreenPoint(transform.position);
+		healthUI.transform.position = pos;
+    }
+
+	public void HPChange(float current, float max)
+	{
+		healthUI.SetText("Health\n{0}/{1}", current, max);
+	}
+}
