50f68c6a8d1e773cb838c208f730038c8b987db9
MItCHeLPL <45109417+MItCHeLPL@users.noreply.github.com>
2019-07-19 16:45:02 +0200

    EnemyDissolve

--- Othreter/Assets/Scripts/Stats/EnemyStats.cs
+++ Othreter/Assets/Scripts/Stats/EnemyStats.cs
@@ -7,6 +7,12 @@ public class EnemyStats : CharacterStats
     Rigidbody rb;
 	PlayerController playerController;
 	EnemyUI enemyUI;
+	[SerializeField]
+	private GameObject model;
+	[SerializeField]
+	private Material dieMaterial;
+
+	private Renderer modelRenderer;
 
 	private void Start()
 	{
@@ -16,6 +22,8 @@ public class EnemyStats : CharacterStats
 		StartCoroutine(CheckArmour());
 
 		RefreshHealthUI();
+
+		modelRenderer = model.gameObject.GetComponent<Renderer>();
 	}
 
 	public override void TakeDamage(int damage) //u can add more events while taking damage only for player
@@ -51,7 +59,13 @@ public class EnemyStats : CharacterStats
 		{
 			playerController.FindEnemy(gameObject, false);
 		}
-		Destroy(gameObject);
+
+		modelRenderer.material = dieMaterial;
+
+		GetComponent<EnemyController>().enabled = false;
+		enemyUI.enabled = false;
+
+		StartCoroutine(Dissolve());
 	}
 
 	private void RefreshHealthUI()
@@ -68,6 +82,23 @@ public class EnemyStats : CharacterStats
 				RefreshHealthUI();
 			}
 			yield return new WaitForSeconds(regenRatePerSecond - 0.1f);
+		}
+	}
+
+	private IEnumerator Dissolve()
+	{
+		int progressId = Shader.PropertyToID("Vector1_CFDFAB28");
+
+		float progress = modelRenderer.material.GetFloat(progressId);
+		while (modelRenderer.material.GetFloat(progressId) < 1)
+		{
+			progress = Mathf.Lerp(progress, 1.0f, Time.deltaTime);
+			modelRenderer.material.SetFloat(progressId, progress);
+			if (progress > 0.75f)
+			{
+				Destroy(gameObject);
+			}
+			yield return new WaitForEndOfFrame();
 		}
 	}
 }
