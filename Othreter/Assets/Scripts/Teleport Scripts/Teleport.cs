a85f8d3a8658f7cfdb0535d72f9ff5f244b91782
Michał Myszor <45109417+MItCHeLPL@users.noreply.github.com>
2022-02-11 23:29:54 +0100

    Improved Demo Scene

--- Othreter/Assets/Scripts/Teleport Scripts/Teleport.cs
+++ Othreter/Assets/Scripts/Teleport Scripts/Teleport.cs
@@ -5,13 +5,32 @@ using UnityEngine;
 public class Teleport : MonoBehaviour
 {
 	[SerializeField]
-	private Vector3 place = default;
+	private Vector3 defaultPlace = Vector3.zero;
 
     private void OnTriggerEnter(Collider col)
     {
         if (col.tag == "Player") //if colides with player
         {
-            col.transform.position = place; //teleport player to spawn
+            col.transform.position = defaultPlace; //teleport player to spawn
         }
+    }
+
+    public void TeleportPlayer()
+    {
+        ObjectsMenager.instance.player.transform.position = defaultPlace;
+    }
+    public void TeleportPlayer(Vector3 newPosition)
+	{
+        ObjectsMenager.instance.player.transform.position = newPosition;
+    }
+    public void TeleportPlayer(Vector3 newPosition, Quaternion newRotation)
+    {
+        ObjectsMenager.instance.player.transform.position = newPosition;
+        ObjectsMenager.instance.player.transform.rotation = newRotation;
+    }
+    public void TeleportPlayer(Transform newTransform)
+    {
+        ObjectsMenager.instance.player.transform.position = newTransform.position;
+        ObjectsMenager.instance.player.transform.rotation = newTransform.rotation;
     }
 }
